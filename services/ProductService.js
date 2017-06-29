c3572a3fc681b627b125045d275d6ef9e90873f3
hyq2015 <18030638805@163.com>
2017-06-29 16:25:35 +0800

    update

--- services/ProductService.js
+++ services/ProductService.js
@@ -1,6 +1,8 @@
 var knex=require("../db/connect.js");
 var elasticsearch = require('elasticsearch');
 var client=require("./ElasticSearchClient");
+
+
 exports.createIndex=function()
 {
     client.getInstance().indices();
